db9ff69afdaddcd3da80fb8ae7238e0529cc36bd
Abu Jayed <aj.webdevlopment@gmail.com>
2023-11-20 22:42:52 +0600

    backend connected to mongodb.

--- index.js
+++ index.js
@@ -1,6 +1,57 @@
 const express = require('express')
+const cors = require("cors");
+require('dotenv').config()
+
 const app = express()
 const port = 3000
+
+const corsConfig = {
+  origin: '*',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+  }
+  app.use(cors(corsConfig))
+  app.options("*", cors(corsConfig))
+
+app.use(express.json());
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://The-Learn-Cloud-job-task:${process.env.DB_PASSWORD}@cluster0.pwifs1n.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+
+    const theLearnCloudJobTaskDB = client.db("the-learn-cloud-job-task")
+    const todoCollection = theLearnCloudJobTaskDB.collection("todoCollection")
+
+
+
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
