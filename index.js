007ae5e1770cbf90ede836f8a1281eee3a73928b
Abu Jayed <aj.webdevlopment@gmail.com>
2023-11-21 03:23:00 +0600

    All Task Requirment full field successfully.

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-const express = require('express')
+const express = require("express");
 const cors = require("cors");
-require('dotenv').config()
+require("dotenv").config();
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 const corsConfig = {
-  origin: '*',
+  origin: "*",
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE']
-  }
-  app.use(cors(corsConfig))
-  app.options("*", cors(corsConfig))
+  methods: ["GET", "POST", "PUT", "DELETE"],
+};
+app.use(cors(corsConfig));
+app.options("*", cors(corsConfig));
 
 app.use(express.json());
 
-
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://The-Learn-Cloud-job-task:${process.env.DB_PASSWORD}@cluster0.pwifs1n.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,21 +24,17 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-
-
-    const theLearnCloudJobTaskDB = client.db("the-learn-cloud-job-task")
-    const todoCollection = theLearnCloudJobTaskDB.collection("todoCollection")
+    const theLearnCloudJobTaskDB = client.db("the-learn-cloud-job-task");
+    const todoCollection = theLearnCloudJobTaskDB.collection("todoCollection");
 
     //getting todos
     app.get("/", async (req, res) => {
-      const result = await todoCollection
-        .find({})
-        .toArray();
+      const result = await todoCollection.find({}).toArray();
       res.send(result);
     });
 
@@ -48,7 +42,7 @@ async function run() {
     app.post("/addTodo", async (req, res) => {
       const body = req.body;
       body.createdAt = new Date();
-      body.status = "uncompleted"
+      body.status = "uncompleted";
       // console.log(body);
       const result = await todoCollection.insertOne(body);
       if (result?.insertedId) {
@@ -61,50 +55,90 @@ async function run() {
       }
     });
 
-
-
-
-
-
-
-
-
-    app.put('/updateOrder', async (req, res) => {
+    // update order after dragging table raw
+    app.put("/updateOrder", async (req, res) => {
       const { newOrder } = req.body;
-    
+
       // console.log('Received new order:', newOrder);
-    
+
       try {
         // Iterate through the new order array and update the order for each item
         for (let i = 0; i < newOrder.length; i++) {
           const itemId = newOrder[i];
-          const realId = {_id: new ObjectId(itemId)}
-          console.log("here is id",itemId);
-          const updatedDoc = {$set: {order: Number(i)+1}}
+          const realId = { _id: new ObjectId(itemId) };
+          console.log("here is id", itemId);
+          const updatedDoc = { $set: { order: Number(i) + 1 } };
           console.log("Before");
-          const result = await todoCollection.updateOne(realId,updatedDoc)
+          const result = await todoCollection.updateOne(realId, updatedDoc);
           console.log("After");
         }
-    
+
         // console.log("success");
-        res.status(200).json({ message: 'Order updated successfully' });
+        res.status(200).json({ message: "Order updated successfully" });
       } catch (error) {
-        console.error('Error updating order:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        console.error("Error updating order:", error);
+        res.status(500).json({ error: "Internal Server Error" });
       }
     });
 
+    // update status after clicking on the checkbox
+    app.put("/updateStatus", async (req, res) => {
+      const { status, id } = req.body;
+      let newStatus;
+      // console.log(status,id);
 
+      try {
+        // Iterate through the new order array and update the order for each item
+        if (status === "uncompleted") {
+          newStatus = "completed";
+        } else {
+          newStatus = "uncompleted";
+        }
+        const realId = { _id: new ObjectId(id) };
+        const updatedDoc = { $set: { status: newStatus } };
+        const result = await todoCollection.updateOne(realId, updatedDoc);
 
+        // console.log("success");
+        res.status(200).json({ message: "Order updated successfully" });
+      } catch (error) {
+        console.error("Error updating order:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
 
+    // make title line through after clicking on it.
+    app.put("/isLine", async (req, res) => {
+      const { isLine, id } = req.body;
+      console.log("Is Line", isLine);
+      let newline;
+      // console.log(status,id);
 
+      try {
+        // Iterate through the new order array and update the order for each item
+        if (isLine === "true") {
+          newline = "false";
+        } else {
+          newline = "true";
+        }
+        const realId = { _id: new ObjectId(id) };
+        const updatedDoc = { $set: { line: newline } };
+        const result = await todoCollection.updateOne(realId, updatedDoc);
 
+        // console.log("success");
+        res.status(200).json({ message: "Order updated successfully" });
+      } catch (error) {
+        console.error("Error updating order:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
 
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -112,13 +146,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
